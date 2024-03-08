a7b6792aca28b2976a372b8232c5f0a22edd40eb
Felipe Laera <felipe.laera@skyone.solutions>
2024-03-08 09:43:52 -0300

    fixing order-tags

--- src/helpers/order-tags.js
+++ src/helpers/order-tags.js
@@ -2,12 +2,14 @@ const convertTagToNumber = (tag) => {
   const [version, rc] = tag.split("-");
 
   const paths = version.replace("v", "").split(".");
-
+  
   //padding zeros to handle modifiers.
   let number = Number(
     paths.reduce(
       (acc, number, index) =>
-        acc + Number(number) * ((paths.length - index) * 1000),
+        acc +
+        Number(number) *
+          Number(`1${new Array(paths.length - index).fill("0").join("")}`),
       0
     )
   );
